84ba20629af27494555c3ff2bbc994255aaf60b1
darksouul <127728307+darksouul@users.noreply.github.com>
2023-03-16 09:11:25 +0530

    Delete contact.js

--- src/functions/contact.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async (event) => {
-  const data = JSON.parse(event.body);
-  // Your code to process the form data goes here
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Form submission successful' })
-  };
-};
