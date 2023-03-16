bb5c9b0cfea82e40f2885b9c8cb7216463807623
darksouul <127728307+darksouul@users.noreply.github.com>
2023-03-16 08:38:39 +0530

    Update ContactMe.vue

--- src/components/ContactMe.vue
+++ src/components/ContactMe.vue
@@ -1,17 +1,22 @@
 <template>
-  <form @submit.prevent="submitForm">
-    <div>
-      <label for="name">Name:</label>
-      <input type="text" id="name" v-model="formData.name" required />
-    </div>
-    <div>
-      <label for="email">Email:</label>
-      <input type="email" id="email" v-model="formData.email" required />
-    </div>
-    <div>
-      <label for="message">Message:</label>
-      <textarea id="message" v-model="formData.message" required></textarea>
-    </div>
+  <form class="contact-form" method="POST" data-netlify data-netlify-site="lambent-caramel-cd3d15">
+    <label for="name">Name</label>
+    <input type="text" name="name" id="name" v-model="formData.name" required />
+    <label for="email">Email</label>
+    <input
+      type="email"
+      name="email"
+      id="email"
+      v-model="formData.email"
+      required
+    />
+    <label for="message">Message</label>
+    <textarea
+      name="message"
+      id="message"
+      v-model="formData.message"
+      required
+    ></textarea>
     <button type="submit">Submit</button>
   </form>
 </template>
@@ -26,31 +31,6 @@ export default {
         message: "",
       },
     };
-  },
-  methods: {
-    submitForm() {
-      const formData = JSON.stringify(this.formData);
-      fetch("/.netlify/functions/submit-contact-form", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: formData,
-      })
-        .then((response) => {
-          alert("Thank you for contacting us!");
-          this.formData = {
-            name: "",
-            email: "",
-            message: "",
-          };
-        })
-        .catch((error) => {
-          alert(
-            "There was a problem submitting your form. Please try again later."
-          );
-        });
-    },
   },
 };
 </script>
